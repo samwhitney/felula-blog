5bb103cfc8f2dcc5b9350ca300c24798b88ec1a5
samwhitney <41234164+samwhitney@users.noreply.github.com>
2022-08-21 20:21:37 +0100

    Add livewire scripts to layout templates

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -4,18 +4,25 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
-
         <title>{{ config('app.name', 'Laravel') }}</title>
 
         <!-- Fonts -->
         <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap">
+		<link href="https://fonts.googleapis.com/css2?family=Playfair Display:wght@400;500;600;700&display=swap" rel="stylesheet">
 
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
+		@livewireStyles
     </head>
     <body>
-        <div class="font-sans text-gray-900 antialiased">
-            {{ $slot }}
+        <div class="min-h-screen bg-gray-100">
+            <!-- Page Content -->
+            <main>
+				<div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+						{{ $slot }}
+				</div>
+            </main>
         </div>
+        @livewireScripts
     </body>
 </html>
